0656d233a53706bb3f047d58d9eec8a0d1afe336
LuanPetruitis <luanpetruitis@hotmail.com>
2023-08-29 19:43:55 -0300

    Adicionando templates

--- curso_django/recipes/views.py
+++ curso_django/recipes/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponse
 
 # Create your views here.
 
+
+# O Django é inteligente já para buscar o arquivo dentro da pasta templates
 def home(request):
-    return HttpResponse("""
-        <!DOCTYPE>
-        <html>
-            <head>
-                <title> Olá mundo</title>
-            </head>
-            <body>
-                Hello World!
-            </body>
-        <html>
-    """)
+    return render(request, "recipes/home.html")
+
 
 def sobre(request):
     return HttpResponse("Hello World!")
+
 
 def contato(request):
     return HttpResponse("Hello World!")
