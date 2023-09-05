ecddaf3a9ffe7a2f0e7fb1d5ebc5c72fdfb777e4
Luan Petruitis <luanpetruitis@hotmail.com>
2023-09-05 16:57:04 -0300

    Renomeando pasta project, ajuste de retorno 404

--- curso_django/recipes/views.py
+++ curso_django/recipes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -16,17 +17,28 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True
+    # ).order_by('-id')
+
+    # if not recipes.exists():
+    #     raise Http404("Not Found")
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category_id=category_id,
         is_published=True
-    ).order_by('-id')
+    ).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, "recipes/pages/recipe-view.html", context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
