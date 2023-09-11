b9b38b8cd3c510112ef69aecd25b86562c26f320
Luan Petruitis <luanpetruitis@hotmail.com>
2023-09-11 16:58:50 -0300

    Testando 100% do projeto

--- curso_django/recipes/models.py
+++ curso_django/recipes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
@@ -23,19 +24,22 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
-
+    cover = models.ImageField(
+        upload_to="recipes/covers/%Y/%m/%d/", blank=True, default=""
+    )
 
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
         default=None,
     )
-    author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True
-    )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
 
 # EDITED
 # title description slug
--- curso_django/recipes/tests/test_recipe_views.py
+++ curso_django/recipes/tests/test_recipe_views.py
@@ -1,7 +1,9 @@
+# @skip("Serve para você pular o teste que quiser")
+# from unittest import skip
+
 from django.urls import resolve, reverse
 
 from recipes import views
-from recipes.models import Recipe
 
 from .test_recipe_base import RecipeTestBase
 
@@ -33,9 +35,23 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn("Recipe Title", content)
         self.assertEqual(len(response_context_recipes), 1)
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show recipe"""
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse("recipes:home"))
+        content = response.content.decode("utf-8")
+        self.assertIn("No recipes found", content)
+
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(reverse("recipes:category", kwargs={"category_id": 1}))
         self.assertIs(view.func, views.category)
+
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = "This is a category test"
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse("recipes:category", args=(1,)))
+        content = response.content.decode("utf-8")
+        self.assertIn(needed_title, content)
 
     def test_recipe_category_view_returns_status_code_404_if_no_recipes_found(self):
         response = self.client.get(
@@ -43,9 +59,24 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show recipe"""
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse("recipes:category", kwargs={"category_id": recipe.category.id})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_views_function_is_correct(self):
         view = resolve(reverse("recipes:recipe", kwargs={"id": 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_template_loads_recipes(self):
+        needed_title = "This is a detail page - It load one recipe"
+        self.make_recipe(title=needed_title)
+        response = self.client.get(reverse("recipes:recipe", kwargs={"id": 1}))
+        content = response.content.decode("utf-8")
+        self.assertIn(needed_title, content)
 
     def test_recipe_detail_view_returns_status_code_404_if_no_recipes_found(self):
         response = self.client.get(reverse("recipes:recipe", kwargs={"id": 100000}))
